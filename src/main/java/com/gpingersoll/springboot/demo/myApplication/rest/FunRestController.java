540584986c92ebcf168fa5404ab93a6b23ac36b3
Greg Ingersoll <gpipuck@gmail.com>
2025-05-27 18:09:53 -0400

    Initial Commit

---
+++ src/main/java/com/gpingersoll/springboot/demo/myApplication/rest/FunRestController.java
@@ -0,0 +1,20 @@
+package com.gpingersoll.springboot.demo.myApplication.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+
+	// Expose / endpoint return "Hello World"
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello World 2!";
+	}
+
+	@GetMapping("/foobar")
+	public String runFoobar() {
+		return "Foobar";
+	}
+
+}
